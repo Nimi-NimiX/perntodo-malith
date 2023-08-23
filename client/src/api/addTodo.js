06ad024297d61bd99244d9d020fbfa2e3a1a41ce
menaka809 <malithjayasekara2255721@gmail.com>
2023-08-23 15:28:18 +0530

    modified add todo api

--- client/src/api/addTodo.js
+++ client/src/api/addTodo.js
@@ -1,10 +1,11 @@
 import React from "react";
+import axios from "axios";
 
 const addTodo = async (description) => {
   try {
     const response = await axios.post(
       "http://localhost:5000/todos",
-      JSON.stringify(body),
+      JSON.stringify(description),
       {
         headers: {
           "Content-Type": "application/json",
