df6f998ddb7f40152a66261667590f3f4401850a
menaka809 <malithjayasekara2255721@gmail.com>
2023-08-23 15:59:26 +0530

    implement seperate api for the delete  todo

--- client/src/api/deleteTodo.js
+++ client/src/api/deleteTodo.js
@@ -1,7 +1,14 @@
 import React from "react";
+import axios from "axios";
 
-const deleteTodo = () => {
-  return <div>deleteTodo</div>;
+const deleteTodo = async (todoId) => {
+  try {
+    const deleteTodo = await axios.delete(
+      `http://localhost:5000/todos/${todoId}`
+    );
+  } catch (err) {
+    throw err;
+  }
 };
 
 export default deleteTodo;
