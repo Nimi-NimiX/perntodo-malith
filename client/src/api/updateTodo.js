04e94120cf53cec580aec158229e9678aafc4e5a
menaka809 <malithjayasekara2255721@gmail.com>
2023-08-23 15:39:47 +0530

    implement seperate api for the update  todo

--- client/src/api/updateTodo.js
+++ client/src/api/updateTodo.js
@@ -1,7 +1,21 @@
 import React from "react";
 
-const updateTodo = () => {
-  return <div>updateTodo</div>;
+const updateTodo = async (todoId, body) => {
+  try {
+    const response = await axios.put(
+      `http://localhost:5000/todos/${todoId}`,
+      body,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 };
 
 export default updateTodo;
