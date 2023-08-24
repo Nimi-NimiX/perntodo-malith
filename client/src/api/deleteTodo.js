9ace118dbdbbd0808579b99c3811fc835d14420a
menaka809 <malithjayasekara2255721@gmail.com>
2023-08-24 11:16:29 +0530

    add base url to the delete todo  API call

--- client/src/api/deleteTodo.js
+++ client/src/api/deleteTodo.js
@@ -1,11 +1,10 @@
 import React from "react";
 import axios from "axios";
+const BASE_URL = process.env.REACT_APP_TODO_BASE_URL;
 
 const deleteTodo = async (todoId) => {
   try {
-    const deleteTodo = await axios.delete(
-      `http://localhost:5000/api/todos/${todoId}`
-    );
+    const deleteTodo = await axios.delete(`${BASE_URL}/${todoId}`);
   } catch (err) {
     throw err;
   }
