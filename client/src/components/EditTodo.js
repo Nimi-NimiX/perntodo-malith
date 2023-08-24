ac1d45f940ae69ccb9c05a929a2f2380460c2908
menaka809 <malithjayasekara2255721@gmail.com>
2023-08-24 18:01:52 +0530

    added MUI component to the Edit Todo component and added some styles

--- client/src/components/EditTodo.js
+++ client/src/components/EditTodo.js
@@ -1,9 +1,33 @@
 import React, { Fragment, useEffect, useState } from "react";
 
+import {
+  Box,
+  Button,
+  Typography,
+  Modal,
+  TextField,
+  Stack,
+  IconButton,
+} from "@mui/material";
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
 const EditTodo = ({ todo }) => {
-  
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   const [description, setDescription] = useState(todo.description);
-  
+
   const updateDesription = async (e) => {
     e.preventDefault();
     try {
@@ -23,7 +47,57 @@ const EditTodo = ({ todo }) => {
   };
   return (
     <Fragment>
-      <button
+      <Button variant="contained" color="secondary" onClick={handleOpen}>
+        Edit
+      </Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <IconButton
+            aria-label="close"
+            onClick={() => setDescription(todo.description)}
+            sx={{
+              position: "absolute",
+              right: 8,
+              top: 8,
+              color: (theme) => theme.palette.grey[500],
+            }}
+          />
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Edit Todo
+          </Typography>
+          <Stack spacing={2} margin={2}>
+            <TextField
+              id="outlined-basic"
+              label="Enter Description"
+              variant="outlined"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          </Stack>
+
+          <Button
+            variant="contained"
+            color="success"
+            sx={{ marginRight: 4, marginLeft: 10 }}
+            onClick={(e) => updateDesription(e)}
+          >
+            Edit
+          </Button>
+          <Button
+            variant="contained"
+            color="warning"
+            onClick={() => setDescription(todo.description)}
+          >
+            Close
+          </Button>
+        </Box>
+      </Modal>
+      {/* <button
         type="button"
         className="btn btn-warning"
         data-toggle="modal"
@@ -80,7 +154,7 @@ const EditTodo = ({ todo }) => {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
     </Fragment>
   );
 };
