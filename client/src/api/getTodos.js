e775c3c55f853af2498f4aafb547e1aaef19e308
menaka809 <malithjayasekara2255721@gmail.com>
2023-08-24 11:18:56 +0530

    add base url to the get  todos  API call

--- client/src/api/getTodos.js
+++ client/src/api/getTodos.js
@@ -1,8 +1,9 @@
 import React from "react";
 import axios from "axios";
+const BASE_URL = process.env.REACT_APP_TODO_BASE_URL;
 
 const getTodos = async () => {
-  const response = await axios.get("http://localhost:5000/api/todos");
+  const response = await axios.get(`${BASE_URL}/todos`);
   const jsonData = response.data;
   return jsonData;
 };
