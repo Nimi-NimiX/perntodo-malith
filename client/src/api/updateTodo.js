4689e779a2e0be98d12f09b13ebc016629fc8cb3
menaka809 <malithjayasekara2255721@gmail.com>
2023-08-24 11:19:22 +0530

    add base url to the update todo  API call

--- client/src/api/updateTodo.js
+++ client/src/api/updateTodo.js
@@ -1,17 +1,14 @@
 import React from "react";
 import axios from "axios";
+const BASE_URL = process.env.REACT_APP_TODO_BASE_URL;
 
 const updateTodo = async (todoId, body) => {
   try {
-    const response = await axios.put(
-      `http://localhost:5000/api/todos/${todoId}`,
-      body,
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
+    const response = await axios.put(`${BASE_URL}/todos/${todoId}`, body, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
     return response.data;
   } catch (error) {
